3c304df23a6b288d44255d7478bcd13d63015855
Rudi Setiawan <rudestewing@gmail.com>
2020-11-05 14:44:09 +0700

    Feat: first fetch query by react

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+app.use(cors());
 mongoose.connect('mongodb://localhost:27017/reactgraphql', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.once('open', () => {
     console.log('connected to mongodb');
